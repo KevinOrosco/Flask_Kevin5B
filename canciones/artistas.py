fe2a54d943c23ecf6e940b3075a213648f38304d
KevinOrosco <kevinorosco411@gmail.com>
2024-05-20 12:19:49 -0300

    Puse que los nombres de los artistas, tengan un link directo a sus canciones

--- canciones/artistas.py
+++ canciones/artistas.py
@@ -1,42 +1,30 @@
 from flask import Flask, render_template,Blueprint
 from . import db
 
-bp = Blueprint('artistas', __name__, url_prefix='/artist')
+bp = Blueprint('artista', __name__, url_prefix='/artist')
 
 @bp.route("/artist")
 def artistas():
     consulta1 = """
-                SELECT Name, Artist_Id FROM artists
-                ORDER BY Name;
-               """
-    consulta2 = """
-                SELECT Name FROM genres
-                ORDER BY Name;
-               """
-    consulta3 = """
-                SELECT Name FROM Tracks
+                SELECT Name, ArtistId FROM artists
                 ORDER BY Name;
                """
     
     con = db.get_db()
     resultado = con.execute(consulta1)
     lista_de_resultados = resultado.fetchall()
-    resultado = con.execute(consulta2)
-    lista_de_generos = resultado.fetchall()
-    resultado = con.execute(consulta3)
-    lista_de_canciones = resultado.fetchall()
-    pagina = render_template("artist.html", artistas = lista_de_resultados, generos = lista_de_generos, canciones = lista_de_canciones)
+    pagina = render_template("artist.html", artistas = lista_de_resultados)
     return pagina
 
 @bp.route("/<int:id>")
 def detalle(id):
     con = db.get_db()
     consulta1 = """
-        SELECT Name, Artist_Id FROM artists
-        WHERE Artist_Id = ? ;
+        SELECT Name, ArtistId FROM artists
+        WHERE ArtistId = ? ;
     """
     consulta2 = """
-        SELECT t.name, a.name, g.Title FROM tracks t
+        SELECT t.name, g.Title FROM tracks t
         JOIN albums g ON t.AlbumId = g.AlbumId
         JOIN artists a ON g.ArtistId = a.ArtistId
         WHERE a.ArtistId = ? ;
