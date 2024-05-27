538d27606c966570d6eb441834204bff45274dd7
Tomy Huisa <Huisatomy2007@gmail.com>
2024-05-27 12:06:38 -0300

    empece a crear el detalle de los albums

--- canciones/album.py
+++ canciones/album.py
@@ -15,3 +15,24 @@ def albums():
     lista_de_albums = resultado.fetchall()
     pagina = render_template("album.html", albums = lista_de_albums)
     return pagina
+
+@bp.route("/<int:id>")
+def album_detalle(id):
+    con = db.get_db()
+    consulta1 = """
+        SELECT Title, AlbumId FROM albums
+        WHERE AlbumId = ? ;
+    """
+    consulta2 = """
+        SELECT a.name, g.Title FROM albums g
+        JOIN tracks a ON g.AlbumId = a.AlbumId
+        WHERE g.AlbumId = ? ;
+    """
+    res = con.execute(consulta1, (id, ))
+    album = res.fetchone()
+    res = con.execute(consulta2, (id, ))
+    lista_canciones = res.fetchall()
+    pagina = render_template("detalle_album.html",
+                             album = album,
+                             canciones = lista_canciones)
+    return pagina
